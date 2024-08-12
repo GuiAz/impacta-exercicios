b1314dd5d5c522d6462e2a52eaee0facae865900
Guilherme <guilherme.cottas@portoseguro.com.br>
2024-08-12 17:02:37 -0300

    Primeiro Exercicio Impacta

---
+++ src/modelo/Carro.java
@@ -0,0 +1,96 @@
+package modelo;
+
+public class Carro {
+	private String marca;
+	private String modelo;
+	private int ano;
+	private String cor;
+	private double preco;
+	private String tipoCombustivel;
+	private int numeroPortas;
+	private boolean arCondicionado;
+	private double quilometragem;
+	private String placa;
+	
+	//Construtor Full
+	public Carro(String marca, String modelo, int ano, String cor, double preco, String tipoCombustivel,
+			int numeroPortas, boolean arCondicionado, double quilometragem, String placa) {
+		this.marca = marca;
+		this.modelo = modelo;
+		this.ano = ano;
+		this.cor = cor;
+		this.preco = preco;
+		this.tipoCombustivel = tipoCombustivel;
+		this.numeroPortas = numeroPortas;
+		this.arCondicionado = arCondicionado;
+		this.quilometragem = quilometragem;
+		this.placa = placa;
+	}
+	
+	//Construtor Vazio
+	public Carro() {
+	}
+	
+	//Getters and Setters
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getTipoCombustivel() {
+		return tipoCombustivel;
+	}
+	public void setTipoCombustivel(String tipoCombustivel) {
+		this.tipoCombustivel = tipoCombustivel;
+	}
+	public int getNumeroPortas() {
+		return numeroPortas;
+	}
+	public void setNumeroPortas(int numeroPortas) {
+		this.numeroPortas = numeroPortas;
+	}
+	public boolean isArCondicionado() {
+		return arCondicionado;
+	}
+	public void setArCondicionado(boolean arCondicionado) {
+		this.arCondicionado = arCondicionado;
+	}
+	public double getQuilometragem() {
+		return quilometragem;
+	}
+	public void setQuilometragem(double quilometragem) {
+		this.quilometragem = quilometragem;
+	}
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+}
---
+++ src/modelo/Principal.java
@@ -0,0 +1,43 @@
+package modelo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Carro meuCarro = new Carro("HYUNDAI", "HB20", 2017, "BRANCO", 58.300, 
+				"ALCOOL/ GASOLINA", 4, true, 27.243, "ABC1234");
+		
+		System.out.println("Marca			: " + meuCarro.getMarca());
+		System.out.println("Modelo			: " + meuCarro.getModelo());
+		System.out.println("Ano				: " + meuCarro.getAno());
+		System.out.println("Cor				: " + meuCarro.getCor());
+		System.out.println("Preço			: " + meuCarro.getPreco());
+		System.out.println("Combustivel		: " + meuCarro.getTipoCombustivel());
+		System.out.println("Portas			: " + meuCarro.getNumeroPortas());
+		System.out.println("Ar Condicionado : " + meuCarro.isArCondicionado());
+		System.out.println("Quilometragem	: " + meuCarro.getQuilometragem());
+		System.out.println("Placa			: " + meuCarro.getPlaca());
+		
+		
+		Carro meuCarro2 = new Carro();
+		meuCarro2.setMarca("FORD");
+		meuCarro2.setModelo("FUSION");
+		meuCarro2.setAno(2014);
+		meuCarro2.setCor("PRATA");
+		meuCarro2.setPreco(77.908);
+		meuCarro2.setTipoCombustivel("ALCOOL/ GASOLINA");
+		meuCarro2.setNumeroPortas(4);
+		meuCarro2.setArCondicionado(false);
+		meuCarro2.setQuilometragem(24.912);
+		meuCarro2.setPlaca("DEF5678");
+		
+		System.out.println("Marca: " + meuCarro2.getMarca() + "," + "\nModelo: " + meuCarro2.getModelo() +
+				", Ano: " + meuCarro2.getAno() + ", Cor: " + meuCarro2.getCor() + ", Preco: " +
+				 meuCarro2.getPreco()	+ ", Combustivel: " + meuCarro2.getTipoCombustivel() +
+				 ", Portas: " + meuCarro2.getNumeroPortas() + ", Ar Condicionado: " + 
+				 meuCarro2.isArCondicionado() + ", Quilometragem: " + meuCarro2.getQuilometragem() +
+				 ", Placa: " + meuCarro2.getPlaca());
+	}
+		
+		
+
+}
